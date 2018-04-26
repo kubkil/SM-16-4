1858b3a51e442fc2ca4a58561552720d8ac0fc52
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-26 22:26:17 +0200

    add div

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,10 @@ import style from '../../styles/Title.css';
 
 // props w nawiasie? chyba bez różnicy
 const Title = props => (
-  <h1 className={style.appTitle}>{props.title}
+  <div>
+    <h1 className={style.appTitle}>{props.title}</h1>
     <p className={style.appNumberOfTasks}>Ilość zadań to: {props.numberoftasks}</p>
-  </h1>
+  </div>
 )
 
 export default Title;
